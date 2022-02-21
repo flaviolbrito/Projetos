ee9907a30e97fd70d35f764901bb2cc533bc3b77
flaviolbrito <flaviolbrito@gmail.com>
2022-02-21 11:09:26 -0300

    update

--- CrudAPITravel/crud.api.travel/crud.api.travel/src/main/java/com/projetos/crud/api/travel/Application.java
+++ CrudAPITravel/crud.api.travel/crud.api.travel/src/main/java/com/projetos/crud/api/travel/Application.java
@@ -3,6 +3,7 @@ package com.projetos.crud.api.travel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Projeto Travel
 @SpringBootApplication
 public class Application {
 
