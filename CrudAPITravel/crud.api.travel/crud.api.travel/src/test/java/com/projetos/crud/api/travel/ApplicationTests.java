03b261345ba3742e64199981e6db47f1054c9a9a
flaviolbrito <46071926+flaviolbrito@users.noreply.github.com>
2022-02-20 23:32:43 -0300

    Inicio Projeto API Crud Travel

---
+++ CrudAPITravel/crud.api.travel/crud.api.travel/src/test/java/com/projetos/crud/api/travel/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.crud.api.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
